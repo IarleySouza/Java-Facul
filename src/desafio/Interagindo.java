efdeb576e9d51643dff1f01b235e3b033d569879
Iarley Souza <iarleysouza615@gmail.com>
2025-02-25 21:59:19 -0300

    update

---
+++ src/desafio/Interagindo.java
@@ -0,0 +1,31 @@
+package desafio;
+
+import javax.swing.*;
+
+public class Interagindo {
+    public static void main(String[] args) {
+        String ex = "";
+
+        for(int a = 1; a <= 10; a++){
+            for(int b = 1; b <= 5; b++){
+                ex += (b + "x" + a + " = " + (b*a) + " \t");
+            }
+            ex +=(" ");
+            ex +=("\n");
+        }
+        ex +=(" ");
+        ex +=("\n");
+
+        for(int a = 1; a <= 10; a++){
+            for(int b = 6; b <= 10; b++){
+                ex +=(b+ "x" +a+ "=" +(b*a)+" \t");
+            }
+            ex += (" ");
+
+            ex += ("\n");
+
+        }
+        JTextArea JtxArea = new JTextArea(ex);
+        JOptionPane.showMessageDialog(null, JtxArea);
+    }
+}
